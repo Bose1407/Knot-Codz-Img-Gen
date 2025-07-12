de7eddba986f788bb901e40acea4ae2273f59392
Bose <bosebeast2@gmail.com>
2025-07-12 11:46:54 +0530

    first commit

---
+++ services/background_service.py
@@ -0,0 +1,6 @@
+def remove_background(api_key, image_data, content_moderation=False):
+    """
+    Dummy background removal function. Replace with actual implementation.
+    """
+    # Return a dummy result_url for testing
+    return {"result_url": "https://dummyimage.com/600x400/ffffff/000000&text=Background+Removed"} 
